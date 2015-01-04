ee024045d788e73ce07c1c655f81bec619ef9cde
Benjamin Streb <Bstreb@gmail.com>
2015-01-03 22:41:45 -0800

    Added some more stuff
    Once again, this code is highly exploratory in nature. I'm imagining
    having some functions that insulate code generation from the Yaml
    front-end I'm using in case I decide to change to something else.

--- ostester/reader.py
+++ ostester/reader.py
@@ -11,5 +11,17 @@ def parse_from_string(string):
     return parse(StringIO(string))
 
 
-def Metadata(header, **kwargs):
+def FileMetadata(header, **kwargs):
     return {"header": header}
+
+
+def FunctionTests(function_name, tests, metadata={}):
+    return {"function": function_name,
+            "tests": tests,
+            "metadata": metadata}
+
+
+def TestCase(declarations, test_arguments, comparison):
+    return {"declarations": declarations,
+            "arguments": arguments,
+            "comparison": comparison}
