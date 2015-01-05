a826253fd14c162e0c50594f0c5b7954b237079f
Benjamin Streb <Bstreb@gmail.com>
2015-01-05 08:43:56 -0800

    Added zeros and pointer tags

--- ostester/yamlreader.py
+++ ostester/yamlreader.py
@@ -13,7 +13,19 @@ def parse_from_string(string):
     return parse(StringIO(string))
 
 
+def zeros(count):
+    return [0]*count
+yaml.add_constructor('!zeros', zeros)
+
+
+class Pointer(yaml.YAMLObject):
+    yaml_tag = '!pointer'
+
+    def __init__(self, data, offset):
+        self.data = data
+        self.offset = offset
+
+
 def transform(yaml):
     pass
-
 
