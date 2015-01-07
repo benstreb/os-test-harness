464391f6ff63aa28b9e48c90a37209e2b5f86e8a
Benjamin Streb <Bstreb@gmail.com>
2015-01-06 17:52:50 -0800

    Got zeros working
    It required only a slight change in my original design. Maybe even one
    for the better, when considering yaml syntax.

--- ostester/yamlreader.py
+++ ostester/yamlreader.py
@@ -16,19 +16,27 @@ def parse_from_string(string):
 
 class Zeros(collections.abc.Sequence):
 
-    def __init__(self, count, *args, **kwargs):
-        print(args, kwargs)
-        self.len = count
+    def __init__(self, len):
+        self.len = len
+
+    @staticmethod
+    def from_loader(loader, node):
+        return Zeros(int(node.value))
 
     def __getitem__(self, i):
+        if i > self.len-1 or i < -self.len:
+            raise IndexError('Zeros index out of range')
         return 0
 
     def __len__(self):
         return self.len
 
+    def __repr__(self):
+        return 'Zeros({})'.format(repr(self.len))
+
 yaml.add_representer(Zeros, lambda dumper, data:
-    dumper.represent_scalar('!zeros', "({})".format(data.len)))
-yaml.add_constructor('!zeros', Zeros)
+    dumper.represent_scalar('!zeros', str(data.len)))
+yaml.add_constructor('!zeros', Zeros.from_loader)
 
 
 class Pointer(yaml.YAMLObject):
