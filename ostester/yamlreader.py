cb4f022fb1fe0780eb2e37c8fdc8ff6a4409115c
Benjamin Streb <Bstreb@gmail.com>
2015-01-11 21:56:59 -0800

    Test and implementation for !ptr+offset loading
    Also changes repr to accomodate the new functionality. The error
    messages this implementation will give are not the best, to say the
    least. That is a feature that will need to happen at a later time.

--- ostester/yamlreader.py
+++ ostester/yamlreader.py
@@ -67,6 +67,8 @@ class Pointer():
     Represents a pointer into an array.
     >>> yaml.load('!ptr value')
     Pointer('value')
+    >>> yaml.load('!ptr array+3')
+    Pointer('array', offset=3)
     >>> yaml.dump(Pointer("value"))
     "!ptr 'value'\\n"
     """
@@ -74,18 +76,23 @@ class Pointer():
 
     def __init__(self, data, offset=0):
         self.data = data
-        self.offset = offset
+        self.offset = int(offset)
 
     @staticmethod
     def from_yaml_loader(loader, node):
-        return Pointer(node.value)
+        args = map(str.strip, node.value.split('+'))
+        return Pointer(*args)
 
     @staticmethod
     def yaml_representer(dumper, data):
         return dumper.represent_scalar(Pointer.yaml_tag, data.data)
 
     def __repr__(self):
-        return 'Pointer({})'.format(repr(self.data))
+        if not self.offset:
+            format_str = 'Pointer({})'
+        else:
+            format_str = 'Pointer({}, offset={})'
+        return format_str.format(repr(self.data), self.offset)
 
 yaml.add_representer(Pointer, Pointer.yaml_representer)
 yaml.add_constructor(Pointer.yaml_tag, Pointer.from_yaml_loader)
