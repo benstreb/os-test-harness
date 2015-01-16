8f8c886c1304d93e7d73ef3da4ec429d889e300e
Benjamin Streb <Bstreb@gmail.com>
2015-01-16 08:23:02 -0800

    More "generated" code
    This is the meat of how I imagine the generated code will look like.
    I've come to a couple of conclusions: thus far, my yaml syntax seems
    like it will be reasonably able to handle the code I'll need to write.
    This is good, it means that my effort in planning out the language was
    not in vain. The other thing I noticed, though, was that I don't think
    that type inference will work as well as I hoped. I think I'm going to
    have to have some sort of type annotation to make this work correctly.

---
+++ ostester/tests/gen/test-compare.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include "compare.h"
+
+uint32_t test_compare()
+{
+    char array0[] = {'a', 'b'};
+    int result0 = compare('a', array0);
+    if (!(result0 < 0))
+    {
+        return 1;
+    }
+    char array1[] = {0, 0, 0};
+    int result1 = compare(0, array1);
+    if (!(result1 == 0))
+    {
+        return 2;
+    }
+    char array2[] = {'c', 'b', 'a'};
+    int result2 = compare('b', array2+1);
+    if (!(result2 > 0))
+    {
+        return 3;
+    }
+}
