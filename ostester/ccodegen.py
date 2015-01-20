7bbece380831a124252fec427a442f44abb06c4b
Benjamin Streb <Bstreb@gmail.com>
2015-01-20 08:12:07 -0800

    Added preliminary codegen functionality
    At this point, this is mostly just a way of making clear the inputs to
    the various templates I'm writing, although I'm also shaking out some
    loose ends, such as the header_to_function_name function.

---
+++ ostester/ccodegen.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, PackageLoader
+
+env = Environment(loader=PackageLoader('ostester', 'templates'))
+
+
+def header_to_function_name(header):
+    pass
+
+
+def render_main(tested_headers):
+    """
+    Returns a string containing the entry point for the generated tests
+    >>> import os
+    >>> metadata = {'header': 'compare.h'}
+    >>> main = render_main((metadata['header'],))
+    >>> assert(main)
+    >>> with open(os.devnull, 'w') as main_file:
+    ...     print(main, file=main_file)
+    """
+    template = env.get_template('main.jinja2.c')
+    tests = (header_to_function_name(h) for h in tested_headers)
+    return template.render(tested_headers, tests)
