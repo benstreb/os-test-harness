bd8f79ee4f00a6a37f91004248b13cb3b6197195
Benjamin Streb <Bstreb@gmail.com>
2015-02-08 22:40:21 -0800

    Added some semicolons to the template

--- ostester/templates/test_case.jinja2.c
+++ ostester/templates/test_case.jinja2.c
@@ -1,10 +1,10 @@
 {% macro test_case(test) %}
 {% for declaration in test.declarations %}
-{{ declaration.c_code() }}
+{{ declaration.c_code() }};
 {% endfor %}
 {% result = newname() %}
 {% fn = test.function %}
-{{ fn.return_type.c_code(result) }} = {{ fn.c_code(args) }}
+{{ fn.return_type.c_code(result) }} = {{ fn.c_code(args) }};
 if (!({{ test.comparison.c_code(result) }}))
 {
     return {{ test.number }};
