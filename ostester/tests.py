858d1c677a8e7cd821950cf70691d3b89112ee4f
Benjamin Streb <Bstreb@gmail.com>
2015-02-19 17:39:13 -0800

    Made ccodegen doctests only show first failure
    At least at the moment, the ccodegen tests all reuse the output of the
    initial render_*_suite function multiple times, causing a number of
    spurious error messages that obscure the actual failure. This change
    prevents that problem.

--- ostester/tests.py
+++ ostester/tests.py
@@ -1,11 +1,12 @@
 import unittest
-from doctest import DocTestSuite
+from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE
 
 def load_tests(loader, tests, ignore):
     from . import yamlreader
     from . import ccodegen
     tests.addTests(DocTestSuite(yamlreader))
-    tests.addTests(DocTestSuite(ccodegen))
+    tests.addTests(DocTestSuite(
+        ccodegen, optionflags=REPORT_ONLY_FIRST_FAILURE))
     return tests
 
 
