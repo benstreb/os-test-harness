742146d249a6d18de2112bbfdaf539389d7da3b1
Benjamin Streb <Bstreb@gmail.com>
2015-02-20 08:50:58 -0800

    Added a BinOp class
    This class is essentially how I'm visualizing code generation for test
    results to look. It's just a prototype, though, so it's subject to
    change.

--- ostester/ast.py
+++ ostester/ast.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 def FileMetadata(header, **kwargs):
     return {"header": header}
 
@@ -12,3 +14,9 @@ def TestCase(declarations, test_arguments, comparison):
     return {"declarations": declarations,
             "arguments": arguments,
             "comparison": comparison}
+
+
+class BinOp(namedtuple('BinOp', ('f', 'arg'))):
+    __slots__ = ()
+    def compare(self, arg):
+        return '{} {} {}'.format(arg, f, self.arg)
