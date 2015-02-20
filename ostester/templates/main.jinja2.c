6395fcfb78d1fbf6881a231be6e17ca3e17095d3
Benjamin Streb <Bstreb@gmail.com>
2015-02-19 17:04:44 -0800

    Fixed a typo in the main template
    Used the loop instead of the loop counter. The perils of dynamically
    typed (or untyped, if you prefer) languages. I'm not switching to
    template haskell at this point, though.

--- ostester/templates/main.jinja2.c
+++ ostester/templates/main.jinja2.c
@@ -1,7 +1,7 @@
 {% from 'suite.jinja2.c' import suite %}
 #include <stdint.h>
 {% for test_header in test_headers %}
-#include "{{ test_headers }}"
+#include "{{ test_header }}"
 {% endfor %}
 
 {{ suite('test_main', test_headers|map('header_to_function_name')) }}
