8f6c714b24f808ebfdfaed36131ccda125c5d825
Benjamin Streb <Bstreb@gmail.com>
2015-02-25 09:09:21 -0800

    Changed output to log, rather than /dev/null
    By default, the log sends its output to /dev/null, but it's easier to
    reconfigure the logger to print stuff than what I had before. Plus, with
    logging, when it does print stuff, doctest doesn't complain with a
    failing test.

--- ostester/ccodegen.py
+++ ostester/ccodegen.py
@@ -12,12 +12,11 @@ env.globals['new_name'] = utils.new_name
 def render_main(tested_headers):
     """
     Returns a string containing the entry point for the generated tests
-    >>> import os
+    >>> import logging
     >>> metadata = {'header': 'compare.h'}
     >>> main = render_main((metadata['header'],))
     >>> assert(main)
-    >>> with open(os.devnull, 'w') as main_file:
-    ...     print(main, file=main_file)
+    >>> logging.getLogger('tests').info(main)
     """
     template = env.get_template('main.jinja2.c')
     return template.render(test_headers=tested_headers)
@@ -26,7 +25,7 @@ def render_main(tested_headers):
 def render_header_suite(header, functions):
     """
     Returns a string containing the entry point for the generated tests
-    >>> import os
+    >>> import logging
     >>> from ostester.yamlreader import Signature
     >>> from ostester.ast import comparisons
     >>> header = 'compare.h'
@@ -39,8 +38,7 @@ def render_header_suite(header, functions):
     ...     }]
     >>> suite = render_header_suite(header, functions)
     >>> assert(suite)
-    >>> with open(os.devnull, 'w') as suite_file:
-    ...     print(suite, file=suite_file)
+    >>> logging.getLogger('tests').info(suite)
     """
     template = env.get_template('header_suite.jinja2.c')
     return template.render(test_header_name=header,functions=functions)
