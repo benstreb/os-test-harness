0ddac1df55f651abb4ee5fb3563a538fa8d58f3e
Benjamin Streb <Bstreb@gmail.com>
2015-02-28 23:20:49 -0800

    Made the tests for CType run again
    It's annoying that I have to manually go through and add modules to the
    test suite in order for their doctests to run. I'm worried that at some
    point I'll forget to do so and tests will be incorrectly skipped.

--- ostester/tests.py
+++ ostester/tests.py
@@ -4,7 +4,9 @@ from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE
 def load_tests(loader, tests, ignore):
     from . import yamlreader
     from . import ccodegen
+    from . import types
     tests.addTests(DocTestSuite(yamlreader))
+    tests.addTests(DocTestSuite(types))
     tests.addTests(DocTestSuite(
         ccodegen, optionflags=REPORT_ONLY_FIRST_FAILURE))
     return tests
