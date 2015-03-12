53f95822d7d69b2f39f7f672101654f13ab20423
Benjamin Streb <Bstreb@gmail.com>
2015-03-12 08:31:51 -0700

    Revert "Renamed SimpleCType to LiteralCType"
    This reverts commit a636882bc07411da394f9fe9a98eae3a14d1375a.
    As I suggested I might, I think I'll refrain from using string literals
    for now, and stick with a simple/array/pointer distinction similar to
    what I was originally planning.

--- ostester/types.py
+++ ostester/types.py
@@ -3,7 +3,7 @@ import re
 
 
 def c_type(type_decl):
-    return _LiteralCType(type_decl)
+    return _SimpleCType(type_decl)
 
 
 class _CType(metaclass=abc.ABCMeta):
@@ -30,19 +30,17 @@ type_formatters = {
 }
 
 
-class _LiteralCType(_CType):
+class _SimpleCType(_CType):
     """
-    Represents a type in C that can be expressed with a literal
-    anywhere. (As opposed to arrays and structs, which can only have
-    literals at instantiation time).
-    >>> t = _LiteralCType("int")
+    Represents a non-composite type in C.
+    >>> t = _SimpleCType("int")
     >>> t
     int
     >>> t.declare("value")
     'int value'
     >>> t.initialize("value", '1')
     'int value = 1'
-    >>> _LiteralCType('char').initialize("value", 'a')
+    >>> _SimpleCType('char').initialize("value", 'a')
     "char value = 'a'"
     """
     type_spec = re.compile(r'(\w+)\s*(\**)\s*((\[\d+\])*)')
