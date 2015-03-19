b28d5be044044f3997fd2d76e8ec3cedaadc986e
Benjamin Streb <Bstreb@gmail.com>
2015-03-19 08:55:33 -0700

    Added initial test for TypeValues
    And made it run

--- ostester/tests.py
+++ ostester/tests.py
@@ -1,12 +1,15 @@
 import unittest
-from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE
+from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE, ELLIPSIS
 
 def load_tests(loader, tests, ignore):
     from . import yamlreader
     from . import ccodegen
     from . import types
+    from . import typevalues
     tests.addTests(DocTestSuite(yamlreader))
     tests.addTests(DocTestSuite(types))
     tests.addTests(DocTestSuite(
         ccodegen, optionflags=REPORT_ONLY_FIRST_FAILURE))
+    tests.addTests(DocTestSuite(
+        typevalues, optionflags=ELLIPSIS))
     return tests
--- ostester/typevalues.py
+++ ostester/typevalues.py
@@ -4,4 +4,8 @@ import abc
 class TypeValue(metaclass=abc.ABCMeta):
     """
     A value and type combo needed for codegen.
+    >>> from . import types
+    >>> TypeValue(1, types.c_type('int'))
+    TypeValue(value=1, type=int, name='...')
     """
+    pass
