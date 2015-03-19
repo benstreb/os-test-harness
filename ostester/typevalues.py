5bcb7edb25d7c78016e762c7583e42432efbd4bd
Benjamin Streb <Bstreb@gmail.com>
2015-03-19 09:00:35 -0700

    Added implementation for TypeValue init+repr test

--- ostester/typevalues.py
+++ ostester/typevalues.py
@@ -8,4 +8,13 @@ class TypeValue(metaclass=abc.ABCMeta):
     >>> TypeValue(1, types.c_type('int'))
     TypeValue(value=1, type=int, name='...')
     """
-    pass
+    def __init__(self, value, type):
+        self.value = value
+        self.type = type
+
+    def __repr__(self):
+        return "TypeValue(value={}, type={}, name='{}')".format(
+            self.value,
+            self.type,
+            None
+        )
