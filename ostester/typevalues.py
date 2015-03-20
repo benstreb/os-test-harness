c04c6978d2294c1998e49342d3f617009a24a02e
Benjamin Streb <Bstreb@gmail.com>
2015-03-20 08:00:00 -0700

    Added test and implementation for assigned names
    This might violate YAGNI, to be honest. I think it will be useful, but
    I'm not sure enough in my design to say for sure.

--- ostester/typevalues.py
+++ ostester/typevalues.py
@@ -7,14 +7,17 @@ class TypeValue(metaclass=abc.ABCMeta):
     >>> from . import types
     >>> TypeValue(1, types.c_type('int'))
     TypeValue(value=1, type=int, name='...')
+    >>> TypeValue(1, types.c_type('int'), name='test')
+    TypeValue(value=1, type=int, name='test')
     """
-    def __init__(self, value, type):
+    def __init__(self, value, type, name=None):
         self.value = value
         self.type = type
+        self.name = name
 
     def __repr__(self):
         return "TypeValue(value={}, type={}, name='{}')".format(
             self.value,
             self.type,
-            None
+            self.name,
         )
