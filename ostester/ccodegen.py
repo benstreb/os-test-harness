568946fd0dcd193ebb1d73a9c4a2656af91a2588
Benjamin Streb <Bstreb@gmail.com>
2015-04-01 17:27:39 -0700

    Changed codegen doctests to unittests

--- ostester/ccodegen.py
+++ ostester/ccodegen.py
@@ -12,11 +12,6 @@ env.globals['new_name'] = utils.new_name
 def render_main(tested_headers):
     """
     Returns a string containing the entry point for the generated tests
-    >>> import logging
-    >>> metadata = {'header': 'compare.h'}
-    >>> main = render_main((metadata['header'],))
-    >>> assert(main)
-    >>> logging.getLogger('tests').info(main)
     """
     template = env.get_template('main.jinja2.c')
     return template.render(test_headers=tested_headers)
@@ -25,21 +20,6 @@ def render_main(tested_headers):
 def render_header_suite(header, functions):
     """
     Returns a string containing the entry point for the generated tests
-    >>> import logging
-    >>> from ostester.yamlreader import Signature
-    >>> from ostester.ast import comparisons
-    >>> header = 'compare.h'
-    >>> functions = [{
-    ...     'name': 'compare',
-    ...     'type': Signature(inputs=['char', 'char*'], output='int'),
-    ...     'tests': [
-    ...         {'args': ['a', ['a', 'b']],
-    ...          'comparison': comparisons['less_than'](0),
-    ...          'number': 1}],
-    ...     }]
-    >>> suite = render_header_suite(header, functions)
-    >>> assert(suite)
-    >>> logging.getLogger('tests').info(suite)
     """
     template = env.get_template('header_suite.jinja2.c')
     return template.render(test_header_name=header,functions=functions)
--- ostester/tests.py
+++ ostester/tests.py
@@ -1,5 +1,36 @@
+import logging
 import unittest
 from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE, ELLIPSIS
+
+from . import ccodegen, yamlreader, ast
+
+
+class CodegenTestCase(unittest.TestCase):
+    def setUp(self):
+        self.parse_tree = [
+            {'header': 'compare.h'},
+            [{'name': 'compare',
+             'type': yamlreader.Signature(inputs=['char', 'char*'],
+                                          output='int'),
+             'tests': [
+                 {'args': ['a', ['a', 'b']],
+                  'comparison': ast.comparisons['less_than'](0),
+                  'number': 1}],
+            }]]
+
+    def test_main_codegen(self):
+        main = ccodegen.render_main((self.parse_tree[0]['header'],))
+        assert main
+        logging.getLogger('tests').info(main)
+
+    def test_header_suite_codegen(self):
+        suite = ccodegen.render_header_suite(
+            self.parse_tree[0]['header'],
+            self.parse_tree[1],
+        )
+        assert suite
+        logging.getLogger('tests').info(suite)
+
 
 def load_tests(loader, tests, ignore):
     from . import yamlreader, ccodegen, types, values, ast
