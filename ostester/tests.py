3eab777c8c8749af16e3957053d8fc5d06f32b36
Benjamin Streb <Bstreb@gmail.com>
2015-04-06 08:57:47 -0700

    Changed yamlreader doctests to unittests
    They fail now, though, since none of the classes have an __eq__method
    and I use that instead of doctest's __repr__.
    In hindsight, maybe I should have done the conversion piecemeal, rather
    than all at once. Live and learn, I suppose

--- ostester/tests.py
+++ ostester/tests.py
@@ -2,7 +2,47 @@ import logging
 import unittest
 from doctest import DocTestSuite, REPORT_ONLY_FIRST_FAILURE, ELLIPSIS
 
-from . import ccodegen, yamlreader, ast, values
+import yaml
+
+from . import ast, ccodegen, types, values, yamlreader
+
+
+class YAMLParseTestCase(unittest.TestCase):
+    def test_integration_parse(self):
+        with open("ostester/tests/test-compare.yaml", 'r') as f:
+            yml = yamlreader.parse(f)
+
+    def test_zeroed(self):
+        Zeroed = yamlreader.Zeroed
+        self.assertEqual(yaml.safe_load("!zeroed 5"), Zeroed(5))
+        self.assertEqual(yaml.dump(Zeroed(3)), "!zeroed '3'\n")
+        self.assertEqual(list(Zeroed(7)), [0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(Zeroed(3)[-3], 0)
+        self.assertEqual(Zeroed(3)[-2], 0)
+        self.assertEqual(Zeroed(4)[1:3], [0, 0])
+
+    def test_pointer(self):
+        Pointer = yamlreader.Pointer
+        ptr = yaml.safe_load('!ptr value')
+        self.assertEqual(ptr, Pointer('value'))
+        offset_ptr = yaml.safe_load('!ptr array+3')
+        self.assertEqual(offset_ptr, Pointer('array', offset=3))
+        self.assertEqual(yaml.dump(ptr), "!ptr 'value'\n")
+        self.assertEqual(yaml.dump(offset_ptr), "!ptr 'array+3'\n")
+
+    def test_signature(self):
+        Signature = yamlreader.Signature
+        sig = yaml.safe_load('int -> int')
+        self.assertEqual(sig, Signature(inputs=['int'], output='int'))
+        hard_sig = yaml.safe_load('int, char* -> char*')
+        self.assertEqual(hard_sig,
+                         Signature(inputs=['int', 'char*'], output='char*'))
+        self.assertEqual(yaml.dump(sig), "!signature 'int -> int'\n")
+        self.assertEqual(yaml.dump(hard_sig),
+                         "!signature 'int, char* -> char*'\n")
+        from .types import _CType
+        self.assertTrue(all(isinstance(t, types._CType) for t in sig.inputs))
+        self.assertTrue(isinstance(sig.output, types._CType))
 
 
 class ASTTestCase(unittest.TestCase):
--- ostester/yamlreader.py
+++ ostester/yamlreader.py
@@ -1,7 +1,5 @@
 """
 Reads in a yaml file representing a set of tests
->>> with open("ostester/tests/test-compare.yaml", 'r') as f:
-...     yml = parse(f)
 """
 
 import collections.abc
@@ -25,18 +23,6 @@ class ABCYAMLMeta(ABCMeta, type(yaml.YAMLObject)): pass
 class Zeroed(collections.abc.Sequence, yaml.YAMLObject, metaclass=ABCYAMLMeta):
     """
     Represents a zeroed region of memory in C
-    >>> yaml.safe_load("!zeroed 5")
-    Zeroed(5)
-    >>> yaml.dump(Zeroed(3))
-    "!zeroed '3'\\n"
-    >>> list(Zeroed(7))
-    [0, 0, 0, 0, 0, 0, 0]
-    >>> Zeroed(3)[-3]
-    0
-    >>> Zeroed(3)[-2]
-    0
-    >>> Zeroed(4)[1:3]
-    [0, 0]
     """
 
     yaml_loader = yaml.SafeLoader
@@ -70,14 +56,6 @@ class Zeroed(collections.abc.Sequence, yaml.YAMLObject, metaclass=ABCYAMLMeta):
 class Pointer(yaml.YAMLObject):
     """
     Represents a pointer into an array.
-    >>> yaml.safe_load('!ptr value')
-    Pointer('value')
-    >>> yaml.safe_load('!ptr array+3')
-    Pointer('array', offset=3)
-    >>> yaml.dump(Pointer("value"))
-    "!ptr 'value'\\n"
-    >>> yaml.dump(Pointer("array", 2))
-    "!ptr 'array+2'\\n"
     """
     yaml_loader = yaml.SafeLoader
     yaml_tag = '!ptr'
@@ -111,20 +89,6 @@ class Pointer(yaml.YAMLObject):
 class Signature(yaml.YAMLObject):
     """
     Represents the signature of the function under test.
-    >>> yaml.safe_load('int -> int')
-    Signature(inputs=[int], output=int)
-    >>> yaml.safe_load('int, char* -> char*')
-    Signature(inputs=[int, char*], output=char*)
-    >>> yaml.dump(Signature(inputs=['int'], output='int'))
-    "!signature 'int -> int'\\n"
-    >>> yaml.dump(Signature(inputs=['int', 'char*'], output='char*'))
-    "!signature 'int, char* -> char*'\\n"
-    >>> sig = Signature(inputs=['int', 'char*'], output='char*')
-    >>> from .types import _CType
-    >>> all(isinstance(t, _CType) for t in sig.inputs)
-    True
-    >>> isinstance(sig.output, _CType)
-    True
     """
     yaml_loader = yaml.SafeLoader
     yaml_tag='!signature'
