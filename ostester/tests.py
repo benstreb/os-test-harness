268c9f3d2c8d0bb362e053c35d8ba80e9a53e8ee
Benjamin Streb <Bstreb@gmail.com>
2015-04-28 08:53:55 -0700

    Added logging of the generated code
    And there's some bad news on this front, the generated code is not
    correct

--- ostester/tests.py
+++ ostester/tests.py
@@ -169,8 +169,11 @@ class IntegrationTestCase(unittest.TestCase):
         with open("ostester/tests/test-compare.yaml", 'r') as f:
             yml = yamlreader.parse(f)
         ast_ = ast.transform(yml)
-        ccodegen.render_main([ast_['header']])
-        ccodegen.render_header_suite(ast_['header'], ast_['tests'])
+        test_logger = logging.getLogger('tests')
+        main = ccodegen.render_main([ast_['header']])
+        test_logger.info(main)
+        header = ccodegen.render_header_suite(ast_['header'], ast_['tests'])
+        test_logger.info(header)
 
 
 def load_tests(loader, tests, ignore):
