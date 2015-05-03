d93b1d68cdcab21c80913376e30414e449f6e461
Benjamin Streb <Bstreb@gmail.com>
2015-05-03 12:28:10 -0700

    Changed c_code call to initialize
    To account for the previous name change

--- ostester/templates/test_case.jinja2.c
+++ ostester/templates/test_case.jinja2.c
@@ -1,6 +1,6 @@
 {% macro test_case(fn, test) %}
 {% for declaration in test.declarations %}
-{{ declaration.c_code() }};
+{{ declaration.initialize() }};
 {% endfor %}
 {% set result = new_name() %}
 {{ fn.type.output.declare(result) }} = {{ fn.name }}({{ test.args|join(', ') }});
